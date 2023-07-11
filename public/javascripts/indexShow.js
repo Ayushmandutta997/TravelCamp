2f95e87b43d2fd0f2db96910c311cdbc7c6d09be
Ayushman Dutta <Ayushman746@gmail.com>
2023-07-11 23:11:31 +0530

    removed Viewport function

--- public/javascripts/indexShow.js
+++ public/javascripts/indexShow.js
@@ -1,24 +1,18 @@
 // Function to check if an element is in the viewport
-function isElementInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
-    );
-}
+// function isElementInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
+//     );
+// }
 
 // Function to handle the scroll event
 function handleScroll() {
     const campgrounds = document.querySelectorAll('.campground');
     for (let i = 0; i < campgrounds.length; i++) {
       const campground = campgrounds[i];
-      if (i === 0) {
-        campground.classList.add('animate');
-      } else {
-        if (isElementInViewport(campground)) {
-          campground.classList.add('animate');
-        }
-      }
+      campground.classList.add('animate');
     }
   }
   
